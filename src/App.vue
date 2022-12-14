cdf6edbcd94a6cb43b7439e409020145d56adefe
Lukáš Chylík <lukas.chylik@riganti.cz>
2022-12-14 08:31:23 +0100

    project added

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <Snow />  
+    <img :src="`${bgPath}`" class="app--bg" />
+    <img :src="`${logoPath}`" class="app--logo" />
+
+    <div class="game">
+        <Intro /> 
+    </div>
+  </div>
+</template>
+
+<script>
+import Intro from "@/views/Intro.vue";
+import Snow from "@/components/Snowflakes.vue"
+
+export default {
+  components: {
+    Intro,
+    Snow
+  },
+  setup() {
+    //const company = "chyron"
+    const company = "riganti"
+
+    const logoPath = '/logo-' + company +  '.svg'
+    const bgPath = '/bg-' + company +  '.svg'
+
+    return {
+      logoPath,
+      bgPath
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "@/styles/app.scss";
+@import "@/styles/hexagon.scss";
+@import "@/styles/users.scss";
+</style>
---
+++ src/components/Winner.vue
@@ -0,0 +1,58 @@
+<template lang="html">
+    <div class="winner--overlay">
+        <div class="winner--box">
+            <a href="/">
+                <img :src="require('@/assets/images/trophy.svg')" />
+            </a>
+            <h1>{{players.winner}}</h1>
+        </div>
+        <div class="pyro">
+            <div class="before"></div>
+            <div class="after"></div>
+        </div>
+    </div>
+</template>
+<script>
+import { mapState } from "vuex"
+export default {
+    computed: {
+        ...mapState(['players'])
+    },
+    setup() {
+        
+        var audioWinner = new Audio(require('@/assets/audio/winner.wav'))
+        audioWinner.play()
+    }
+}
+</script>
+<style lang="scss" scoped>
+@import "@/styles/winner.scss";
+    .winner {
+        &--overlay {
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            display: grid;
+            place-items: center;
+            background-color: var(--color-background);
+        }
+
+        &--box {
+            position: relative;
+            width: 500px;
+        }
+    }
+
+    img {
+        width: 100%;
+    }
+
+    h1 {
+        position: absolute;
+        bottom: 5px;
+        left: 50%;
+        transform: translate(-50%, 0);
+    }
+</style>
